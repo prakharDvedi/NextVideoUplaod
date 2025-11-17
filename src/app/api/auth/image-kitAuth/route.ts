e6dafd4616bc164aadacdedaaea03d071a875a18
prakharDvedi <prakhardwivedi.12@gmail.com>
2025-11-17 22:14:04 +0530

    Setup Image Kit

---
+++ src/app/api/auth/image-kitAuth/route.ts
@@ -0,0 +1,28 @@
+// File: app/api/upload-auth/route.ts
+import { getUploadAuthParams } from "@imagekit/next/server";
+import { NextResponse as Response } from "next/server";
+
+export async function GET() {
+  // Your application logic to authenticate the user
+  // For example, you can check if the user is logged in or has the necessary permissions
+  // If the user is not authenticated, you can return an error response
+
+  try {
+    const { token, expire, signature } = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    });
+
+    return Response.json({
+      token,
+      expire,
+      signature,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    });
+  } catch (error) {
+    return Response.json(
+      { error: "Failed to generate upload auth parameters" },
+      { status: 500 }
+    );
+  }
+}
